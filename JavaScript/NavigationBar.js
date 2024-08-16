e549a3966853ab1748e87bc1efd5903cfb4a3156
Amro <2592997@students.wits.ac.za>
2024-08-16 23:19:33 +0200

    Navigation Bar

---
+++ JavaScript/NavigationBar.js
@@ -0,0 +1,30 @@
+const Options = [
+    {name : "Home", href:"../index.html"},
+    {name : "Display", href:""},
+    {name : "Cart", href:""}
+]
+
+export function CreateNavigationBar(PageName){
+    const nav = document.querySelector("header > nav");
+    const ul = document.createElement("ul");
+    Options.forEach(option => {
+        const li = document.createElement("li");
+        li.style.listStyle = "none";
+        if (PageName == option.name){
+            li.innerText = option.name;
+            li.style.textDecoration = "underline";
+        }
+        else{
+            const a = document.createElement("a");
+            a.innerText = option.name;
+            a.setAttribute("href", option.href);
+            li.appendChild(a);
+            a.style.textDecoration = "none";
+            a.style.color = "black";
+        }
+        ul.appendChild(li);
+    });    
+    nav.appendChild(ul);
+    ul.classList.add("NavigationBar");
+}
+
